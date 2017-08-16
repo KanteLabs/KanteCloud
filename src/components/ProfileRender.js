d55e9cda7ca32d18019ff8f0cb3a4cf247c9ce2a
Amidou Kante <kamdiou@gmail.com>
2017-08-16 10:19:31 -0400

    created userprofile, added profile render

--- src/components/ProfileRender.js
+++ src/components/ProfileRender.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
-import ProfileRender from './ProfileRender';
 import {client_id} from './config';
 
-class UserProfile extends Component {
+class ProfileRender extends Component {
     constructor(props){
         super(props);
         this.state = {
@@ -34,4 +33,45 @@ class UserProfile extends Component {
     }
 }
 
-export default UserProfile;
+export default ProfileRender;
+/* 
+
+{
+"id":51562330,
+"kind":"user",
+"permalink":"kehlanimusic",
+"username":"Kehlani",
+"last_modified":"2017/08/12 22:15:32 +0000",
+"uri":"https://api.soundcloud.com/users/51562330",
+"permalink_url":"http://soundcloud.com/kehlanimusic",
+"avatar_url":"https://i1.sndcdn.com/avatars-000271481823-fszt02-large.jpg",
+"country":"United States",
+"first_name":"",
+"last_name":"",
+"full_name":"",
+"description":"Singer/Songwriter \nOakland to Los Angeles\n\nMGMT:\ndavid@abvgrnd.com\ninfo@thetsunamimob.com\n\nBOOKING:\nCaroline Yim @ ICM \nCyim@icmpartners.com",
+"city":"Oakland, CA",
+"discogs_name":null,
+"myspace_name":null,
+"website":null,
+"website_title":null,
+"track_count":95,
+"playlist_count":5,
+"online":false,
+"plan":"Pro Plus",
+"public_favorites_count":7,
+"followers_count":508907,
+"followings_count":0,
+"subscriptions":[
+{
+"product":{
+"id":"creator-pro-unlimited",
+"name":"Pro Unlimited"
+}
+}
+],
+"reposts_count":16
+}
+
+
+*/
