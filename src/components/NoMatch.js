21325e7b5f7e3dfdf0b22589e4e2e52571c24ab0
Amidou Kante <kamdiou@gmail.com>
2017-08-27 21:13:46 -0400

    Redirect working

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom';
 
 class NoMatch extends Component {
-   
-
     componentDidMount(){
         console.log('404')
     }
@@ -11,6 +10,7 @@ class NoMatch extends Component {
     return(
         <div>
             <h1>404 Page Not found :( </h1>
+            <Redirect to='' />
         </div>
         )
     }
