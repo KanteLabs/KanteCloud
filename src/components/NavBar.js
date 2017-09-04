4ad70a2d5c0c3bfd96c33622e02037b8849fc818
Amidou Kante <kamdiou@gmail.com>
2017-09-03 21:42:29 -0400

    bug fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -27,10 +27,10 @@ class NavBar extends Component{
     }
 
     //This will search for the genre tag by using the genreTags provided in the config file
-    handleGenreCall=(event)=>{
-        console.log(event.target.title)
-        let name = (event.target.title);
-        let menuItem = document.querySelector(`button[title = ${name}]`);
+    handleGenreCall(event){
+        console.log(event)
+        let name = (event);
+        let menuItem = document.querySelector(`button[title = "${name}"]`);
         if(menuItem.className === 'nav-item nav-link'){
             document.querySelectorAll('button.nav-item.nav-link').forEach((item)=>{
                 item.className = 'nav-item nav-link';
@@ -85,12 +85,12 @@ class NavBar extends Component{
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                <button className="nav-item nav-link" title="Pop" onTouch={(event) =>this.handleGenreCall(event)} onClick={(event) =>this.handleGenreCall(event)}>Pop</button>
-                <button className="nav-item nav-link" title="Hip-Hop" onTouchStart={(event) =>this.handleGenreCall(event)} onClick={(event) =>this.handleGenreCall(event)}>Hip-Hop</button>
-                <button className="nav-item nav-link" title="Reggae" onTouchEnd={(event) =>this.handleGenreCall(event)} onClick={(event) =>this.handleGenreCall(event)}>Reggae</button>
-                <button className="nav-item nav-link" title="R&B" onTouchStartCapture={(event) =>this.handleGenreCall(event)} onClick={(event) =>this.handleGenreCall(event)}>R&B</button>
-                <button className="nav-item nav-link" title="EDM" onClick={(event) =>this.handleGenreCall(event)}>EDM</button>
-                <button className="nav-item nav-link" title="Dubstep" onClick={(event) =>this.handleGenreCall(event)}>Dubstep</button>
+                <button className="nav-item nav-link" title="Pop" onClick={(event) =>this.handleGenreCall(event.target.title)}>Pop</button>
+                <button className="nav-item nav-link" title="Hip-Hop" onClick={(event) =>this.handleGenreCall(event.target.title)}>Hip-Hop</button>
+                <button className="nav-item nav-link" title="Reggae" onClick={(event) =>this.handleGenreCall(event.target.title)}>Reggae</button>
+                <button className="nav-item nav-link" title="R&B" onClick={(event) =>this.handleGenreCall(event.target.title)}>R&B</button>
+                <button className="nav-item nav-link" title="EDM" onClick={(event) =>this.handleGenreCall(event.target.title)}>EDM</button>
+                <button className="nav-item nav-link" title="Dubstep" onClick={(event) =>this.handleGenreCall(event.target.title)}>Dubstep</button>
                 <button className="nav-item nav-link navItem" onClick={() =>this.handleLatestTracksClick()}>Latest</button>
                 <button className="nav-item nav-link loginItem" onClick={() =>this.handleLoginClick()}>Login</button>
                 </div>
